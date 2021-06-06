2ab2ab3a2dae3ccc0b5c238fe1cd7b12afc715df
Gabriel <gabrielfranccelino@gmail.com>
2021-06-06 10:28:31 -0300

    test: add and getTotal to cart shopping

--- src/lib/Cart.js
+++ src/lib/Cart.js
@@ -1,3 +1,13 @@
-export default class Cart{
-    
+export default class Cart {
+  items = [];
+
+  add(item) {
+    this.items.push(item);
+  }
+
+  getTotal() {
+    return this.items.reduce((total, item) => {
+      return total + item.quantity * item.product.price;
+    }, 0);
+  }
 }
