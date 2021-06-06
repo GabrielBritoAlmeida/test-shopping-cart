bb5e4d8b2ac9e7a7c32dd16dae1afcd527db94c2
Gabriel <gabrielfranccelino@gmail.com>
2021-06-06 18:58:57 -0300

    test: should ensure no more than on product exist at a time

--- src/lib/Cart.js
+++ src/lib/Cart.js
@@ -2,12 +2,25 @@ export default class Cart {
   items = [];
 
   add(item) {
-    this.items.push(item);
+    const indexItem = this.items.findIndex(
+      (itemIndex) => itemIndex.product.title === item.product.title
+    );
+
+    if (indexItem >= 0) {
+      this.items[indexItem].quantity++;
+    } else {
+      this.items.push(item);
+    }
   }
 
   getTotal() {
     return this.items.reduce((total, item) => {
       return total + item.quantity * item.product.price;
     }, 0);
+  }
+
+  getTotalItems() {
+    console.log("🚀 ~ file: Cart.js ~ line 25 ~ Cart ~ getTotalItems ~ this.items", this.items)
+    return this.items.length;
   }
 }
